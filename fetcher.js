6d785c09380a14df805710dbc277549034c36b33
John <johnartuz@hotmail.com>
2021-12-02 12:31:47 -0500

    remove unecessary imports

--- fetcher.js
+++ fetcher.js
@@ -1,10 +1,5 @@
 const request = require("request");
 const fs = require("fs");
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 
 const input = process.argv.slice(2);
 const targetLink = input[0];
