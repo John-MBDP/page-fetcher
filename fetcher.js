21c422fe9971449b9c05db6c89b26585379564c8
John <johnartuz@hotmail.com>
2021-12-02 12:31:06 -0500

    Http request

---
+++ fetcher.js
@@ -0,0 +1,26 @@
+const request = require("request");
+const fs = require("fs");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = process.argv.slice(2);
+const targetLink = input[0];
+const filepath = input[1];
+
+request(`https://${targetLink}/`, (error, response, body) => {
+  if (error) {
+    console.log("error:", error); // Print the error if one occurred
+  }
+  fs.writeFile(`${filepath}`, body, (err) => {
+    if (err) {
+      console.error("error", err);
+    } else {
+      console.log(
+        `Downloaded and saved ${response.headers["content-length"]} bytes to ${filepath}`
+      );
+    }
+  });
+});
